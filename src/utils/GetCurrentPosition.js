91c9eab6c93e6dffdfb96e59505968103a31e24b
Ramon Julia <reimon1972@gmail.com>
2020-01-12 11:29:29 +0800

    change port

--- src/utils/GetCurrentPosition.js
+++ src/utils/GetCurrentPosition.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { connect } from "react-redux";
 import { getPosition } from "../services/redux/actions/settings.actions";
 import { geolocated } from "react-geolocated";
