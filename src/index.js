30d3526ab3b85b75c1aad81a4e4b682a89df3500
Ramon Julia <reimon1972@gmail.com>
2020-01-13 11:56:45 +0800

    delete websocket

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Provider } from 'react-redux';
 import store from './services/redux/store'
 import './index.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
 
 let linkApi;
 
@@ -45,7 +45,3 @@ ReactDOM.render(
   </ApolloProvider>
   , document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
