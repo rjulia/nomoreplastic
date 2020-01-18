df3aef6da6dcb402e8711aa80127e9346a17f3bf
Ramon Julia <reimon1972@gmail.com>
2020-01-18 12:27:56 +0800

    create still working add and contact sections

--- src/pages/addPage/index.js
+++ src/pages/addPage/index.js
@@ -1,10 +1,54 @@
 import React from 'react'
 import localization from "../../localization"
+import styled from 'styled-components';
+import AddPicture from "../../assets/images/add.jpg";
+
+const Wrapper = styled('div')`
+  position: relative;
+  display: block;
+  width: calc(100vw - 100px);
+  height: 100%;
+  margin:0;
+  padding: 0;
+  div {
+    position: relative;
+    width: calc(100% - 80px);
+    height: calc(100vh - 80px);
+    margin: 40px;
+    border-radius: 40px;
+    overflow: hidden;
+  }
+  div img {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: -100;
+    
+  }
+  p {
+    margin:0;
+    font-size: 100px;
+    font-weight: 300;
+    position:absolute;
+    left: 50%;
+    top: 50%;
+    text-align: center;
+    transform: translate(-50%, -50%);
+    color: #fff;
+    text-shadow: 1px 1px 10px #000000;
+  }
+
+`
+
 const Add = () => {
   return (
-    <div>
-      {localization.home.add}
-    </div>
+    <Wrapper>
+      <p>We are still working. Thanks</p>
+      <div>
+        <img src={AddPicture} alt="add section" />
+
+      </div>
+    </Wrapper>
   )
 }
 
--- src/pages/contactPage/index.js
+++ src/pages/contactPage/index.js
@@ -1,10 +1,53 @@
 import React from 'react'
+import styled from 'styled-components';
+import Phone from "../../assets/images/phone.jpg";
+
+const Wrapper = styled('div')`
+  position: relative;
+  display: block;
+  width: calc(100vw - 100px);
+  height: 100%;
+  margin:0;
+  padding: 0;
+  div {
+    position: relative;
+    width: calc(100% - 80px);
+    height: calc(100vh - 80px);
+    margin: 40px;
+    border-radius: 40px;
+    overflow: hidden;
+  }
+  div img {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: -100;
+    
+  }
+  p {
+    margin:0;
+    font-size: 100px;
+    font-weight: 300;
+    position:absolute;
+    left: 50%;
+    top: 50%;
+    text-align: center;
+    transform: translate(-50%, -50%);
+    color: #fff;
+    text-shadow: 1px 1px 10px #000000;
+  }
+
+`
 
 const Contact = () => {
   return (
-    <div>
-      Contact
-    </div>
+    <Wrapper>
+      <p>We are still working. Thanks</p>
+      <div>
+        <img src={Phone} alt="add section" />
+
+      </div>
+    </Wrapper>
   )
 }
 
