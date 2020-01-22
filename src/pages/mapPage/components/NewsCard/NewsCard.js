0041838e1f2ba7d7fdce2845dd8883601fc49df2
Ramon Julia <reimon1972@gmail.com>
2020-01-22 08:29:18 +0800

    add external link to news

--- src/pages/mapPage/components/NewsCard/NewsCard.js
+++ src/pages/mapPage/components/NewsCard/NewsCard.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 import { Title, Paragraph } from '../../../../components'
 import Waste from '../../../../assets/images/beach-plastic.webp';
 import './NewsCard.scss';
 import moment from "moment";
 
 const NewsCard = ({ item }) => {
-  const { title, date, description, urlToImage, imageUrl, content__zh, content__en } = item
+  const { title, date, description, urlToImage, imageUrl, content__zh, content__en, url } = item
   return (
     <div className="newscard">
       <div className="news-card__image">
@@ -15,7 +16,9 @@ const NewsCard = ({ item }) => {
       </div>
       <div className="news-card__content">
         {date ? <Paragraph tag="span" text={moment(date).format('MM/DD/YY')} classN="news-card__date" /> : ''}
-        <Title tag="h3" text={title} classN="news-card__title" />
+        <a href={url} target="_blank" rel="noopener noreferrer">
+          <Title tag="h3" text={title} classN="news-card__title" />
+        </a>
         <Paragraph tag="p" isTrucate={true} text={description || content__zh ||
           content__en} classN="news-card__text" />
       </div>
