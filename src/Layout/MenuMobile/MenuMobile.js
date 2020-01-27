109599208ad8e4049c4847826cc29362b6ef84f5
Ramon Julia <reimon1972@gmail.com>
2020-01-27 11:13:11 +0800

    add icon in mobile menu

--- src/Layout/MenuMobile/MenuMobile.js
+++ src/Layout/MenuMobile/MenuMobile.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import { connect } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { HomeIcon, EmailIcon, AddIcon } from "../../assets/icons";
+import { ButtonMenu } from "../../components";
+import localization from "../../localization";
+import { toggleMenu } from "../../services/redux/actions/settings.actions";
+
 
 import './MenuMobile.scss';
 
-const MenuMobile = ({ isOpen }) => {
+const MenuMobile = ({ isOpen, onToggleMenu }) => {
   const IsMenuOpen = (isOpen) ? 'isOpen' : '';
+  const handelToggleMenu = () => {
+    onToggleMenu()
+  }
   return (
     <div className={`${IsMenuOpen} menu-mobile`}>
-      hello
+      <div className="menu__buttons">
+        <NavLink onClick={handelToggleMenu} to="/map">
+          <ButtonMenu text={localization.home.home}>
+            <HomeIcon width="60px" />
+          </ButtonMenu>
+        </NavLink>
+        <NavLink onClick={handelToggleMenu} to="contact">
+          <ButtonMenu text={localization.home.contact}>
+            <EmailIcon width="60px" />
+          </ButtonMenu>
+        </NavLink>
+        <NavLink onClick={handelToggleMenu} to="add">
+          <ButtonMenu text={localization.home.add}>
+            <AddIcon width="60px" />
+          </ButtonMenu>
+        </NavLink>
+
+      </div>
     </div>
   )
 }
@@ -16,6 +42,10 @@ const mapStateToProps = state => ({
   isOpen: state.settings.isOpenMenu
 })
 
+const mapDispatchToProps = dispatch => ({
+  onToggleMenu: () => dispatch(toggleMenu())
+})
 
 
-export default connect(mapStateToProps)(MenuMobile) 
+
+export default connect(mapStateToProps, mapDispatchToProps)(MenuMobile) 
--- src/pages/contactPage/index.js
+++ src/pages/contactPage/index.js
@@ -5,15 +5,15 @@ import Phone from "../../assets/images/phone.jpg";
 const Wrapper = styled('div')`
   position: relative;
   display: block;
-  width: calc(100vw - 100px);
+  width: 100%;
   height: 100%;
   margin:0;
   padding: 0;
   div {
     position: relative;
     width: calc(100% - 80px);
-    height: calc(100vh - 80px);
-    margin: 40px;
+    height: calc(100vh - 168px);
+    margin: 128px 40px 40px;
     border-radius: 40px;
     overflow: hidden;
   }
@@ -26,7 +26,7 @@ const Wrapper = styled('div')`
   }
   p {
     margin:0;
-    font-size: 100px;
+    font-size: 45px;
     font-weight: 300;
     position:absolute;
     left: 50%;
@@ -35,6 +35,19 @@ const Wrapper = styled('div')`
     transform: translate(-50%, -50%);
     color: #fff;
     text-shadow: 1px 1px 10px #000000;
+  }
+  @media (min-width: 1024px) {
+    width: calc(100vw - 100px);
+    margin-left: 100px;
+    div {
+      width: calc(100% - 80px);
+      height: calc(100vh - 80px);
+      margin: 40px;
+
+    }
+    p {
+      font-size: 100px;
+    }
   }
 
 `
