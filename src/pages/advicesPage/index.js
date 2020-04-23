eae38f28c42977f1833eefdc02b1df78d52f5c8b
Ramon Julia <reimon1972@gmail.com>
2020-04-23 17:32:15 +0800

    add advice detail 2

---
+++ src/pages/advicesPage/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import './advices.scss';
+import { useQuery } from '@apollo/react-hooks';
+import { Spinner, Title, Paragraph } from "../../components";
+import CardAdvice from "./components/CardAdvice/CardAdvice";
+import { ADVICES_QUERY } from "../../services/apollo/queries";
+import { useTranslation } from 'react-i18next';
+import variables from '../../scss/variables.scss';
+
+const Advices = () => {
+
+  const { t } = useTranslation();
+  const { data, loading, error } = useQuery(ADVICES_QUERY);
+  if (loading) return <Spinner className="spinner__map" />;
+  if (error) return <p>ERROR</p>;
+  console.log(data)
+  return (
+    <div className="advice__container">
+      <div className="advice__title">
+        <Title
+          tag={'h1'}
+          text={t('advice.title')}
+          size={48}
+          color={variables.primary}
+        />
+        <Paragraph
+          tag='p'
+          classN="contact__paragraph"
+          text={t('advice.paraf')}
+        />
+      </div>
+      <div className="row">
+
+        <div className="row">
+          {
+            data.getAdvices.map(advice => (
+              <div key={advice.id} className="column">
+                <CardAdvice advice={advice} />
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Advices
